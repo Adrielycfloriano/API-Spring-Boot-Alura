5caa18b1d2ec678bbc921840ddebc18c6a505dee
Adriely Floriano <adriely.floriano@azulseguros.com.br>
2024-02-15 12:20:30 -0300

    feat Documentação/Testes: Aula 04: Testes Automatizados

--- src/main/java/med/voll/api/domain/consulta/Consulta.java
+++ src/main/java/med/voll/api/domain/consulta/Consulta.java
@@ -10,34 +10,34 @@ import med.voll.api.domain.paciente.Paciente;
 
 import java.time.LocalDateTime;
 
-    @Table(name = "consultas")
-    @Entity(name = "Consulta")
-    @Getter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @EqualsAndHashCode(of = "id")
-    public class Consulta {
+@Table(name = "consultas")
+@Entity(name = "Consulta")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Consulta {
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        @ManyToOne(fetch = FetchType.LAZY)
-        @JoinColumn(name = "medico_id")
-        private Medico medico;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medico_id")
+    private Medico medico;
 
-        @ManyToOne(fetch = FetchType.LAZY)
-        @JoinColumn(name = "paciente_id")
-        private Paciente paciente;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "paciente_id")
+    private Paciente paciente;
 
-        private LocalDateTime data;
+    private LocalDateTime data;
 
-        @Column(name = "motivo_cancelamento")
-        @Enumerated(EnumType.STRING)
-        private MotivoCancelamento motivoCancelamento;
+    @Column(name = "motivo_cancelamento")
+    @Enumerated(EnumType.STRING)
+    private MotivoCancelamento motivoCancelamento;
 
-        public void cancelar(MotivoCancelamento motivo) {
-            this.motivoCancelamento = motivo;
-        }
-
+    public void cancelar(MotivoCancelamento motivo) {
+        this.motivoCancelamento = motivo;
     }
+
+}
